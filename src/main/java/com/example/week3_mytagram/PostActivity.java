fbcb5978663bef93b0a3b6fc85f12a19daeba75a
U180709057 <burak_paar@hotmail.com>
2022-10-07 23:47:12 +0300

    Initial commit

---
+++ src/main/java/com/example/week3_mytagram/PostActivity.java
@@ -0,0 +1,90 @@
+package com.example.week3_mytagram;
+
+import static android.bluetooth.BluetoothClass.Service.CAPTURE;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+
+public class PostActivity extends AppCompatActivity {
+
+    ImageView img;
+    private EditText txtMsg;
+    private ImageButton btnOk;
+    private ImageButton btnCancel;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+
+        final ActivityResultLauncher<Intent> launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult()
+                , new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Bundle bundle = result.getData().getExtras();
+                            Bitmap image = (Bitmap) bundle.get("data");
+                            img.setImageBitmap(image);
+
+
+                        }
+                    }
+                });
+
+        img = findViewById(R.id.imageView);
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                launcher.launch(intent);
+            }
+        });
+
+        txtMsg = (EditText)findViewById(R.id.txtMessage);
+        btnOk = (ImageButton) findViewById(R.id.btnOk);
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                Bundle bundle = new Bundle();
+                bundle.putCharSequence("msg",txtMsg.getText());
+                bundle.putParcelable("bitmap",((BitmapDrawable)img.getDrawable()).getBitmap());
+                intent.putExtras(bundle);
+                setResult(Activity.RESULT_OK,intent);
+                finish();
+            }
+        });
+        btnCancel = (ImageButton) findViewById(R.id.btnCancel);
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setResult(Activity.RESULT_CANCELED);
+                finish();
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE && resultCode == Activity.RESULT_OK) {
+            Bundle bundle = data.getExtras();
+            Bitmap image = (Bitmap) bundle.get("data");
+            img.setImageBitmap(image);
+        }
+    }}
---
+++ src/test/java/com/example/week3_mytagram/PostActivityTest.java
@@ -0,0 +1,7 @@
+package com.example.week3_mytagram;
+
+import junit.framework.TestCase;
+
+public class PostActivityTest extends TestCase {
+
+}
